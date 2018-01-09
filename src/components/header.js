3375599454b7b6256ef996b81cde71ec0301f980
12masta <stanek.marcinp@gmail.com>
2018-01-10 00:12:00 +0100

    font size styling

--- src/components/header.js
+++ src/components/header.js
@@ -3,9 +3,17 @@ import React from 'react';
 import { Text } from 'react-native';
 
 // make a Component
-const Header = () => (
-  <Text>Albums!</Text>
-);
+const Header = () => {
+  const { textStyle } = styles;
+
+  return <Text style={textStyle}>Albums!</Text>;
+};
+
+const styles = {
+  textStyle: {
+    fontSize: 20
+  }
+};
 
 // make the componenet available to other parts of the App
 export default Header;
