dc40e84a495546a4c446615390ab5af03f83d84d
12masta <stanek.marcinp@gmail.com>
2018-01-09 23:51:53 +0100

    Header component and reusable implemented

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 // import a library to help create a Component
 import React from 'react';
-import { Text, AppRegistry } from 'react-native';
+import { AppRegistry } from 'react-native';
+import Header from './src/components/header';
 
 // create a  Component
 const App = () => (
-  <Text>Some text</Text>
+  <Header />
 );
 
 // Render it on device
---
+++ src/components/header.js
@@ -0,0 +1,11 @@
+//import libraries for making a Component
+import React from 'react';
+import { Text } from 'react-native';
+
+// make a Component
+const Header = () => (
+  <Text>Albums!</Text>
+);
+
+// make the componenet available to other parts of the App
+export default Header;
