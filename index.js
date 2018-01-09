40dac0fcc6ab757b106b6a3490428942e60eda82
12masta <stanek.marcinp@gmail.com>
2018-01-09 23:29:15 +0100

    Hello world

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-import { AppRegistry } from 'react-native';
-import App from './App';
+// import a library to help create a Component
+import React from 'react';
+import { Text, AppRegistry } from 'react-native';
 
+// create a  Component
+const App = () => (
+  <Text>Some text</Text>
+);
+
+// Render it on device
 AppRegistry.registerComponent('albums', () => App);
