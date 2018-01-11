ffa5a6e41f9a97187db135c62073ce7597b3b596
12masta <stanek.marcinp@gmail.com>
2018-01-12 00:40:33 +0100

    AlbumList init and show on device

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 // import a library to help create a Component
 import React from 'react';
-import { AppRegistry } from 'react-native';
+import { AppRegistry, View } from 'react-native';
 import Header from './src/components/header';
-
+import AlbumList from './src/components/AlbumList';
 // create a  Component
-const App = () => (
-  <Header headerText={'Albums'} />
-);
+const App = () => {
+    return (
+      <View>
+        <Header headerText={'Albums'} />
+        <AlbumList />
+      </View>
+  );
+};
 
 // Render it on device
 AppRegistry.registerComponent('albums', () => App);
